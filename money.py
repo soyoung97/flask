0a9d8506944156b4a00732a6ad20890f73658089
Ubuntu <soyoung@madcamp.i5zx4j1sd4we1arp1grvezl5hc.psx.internal.cloudapp.net>
2018-07-18 06:35:05 +0000

    Organized overall code.

--- money.py
+++ money.py
@@ -5,48 +5,119 @@ import sqlite3 as sql
 import ast
 from werkzeug.datastructures import ImmutableMultiDict
 from werkzeug import secure_filename
+from pyfcm import FCMNotification
+
+
+
 app = Flask(__name__)
+
+def printit(str):
+    print("----------------------------Inside %s function---------------------------------"%str)
+
+def getdata(data):
+    print(" got data ")
+    ret = ast.literal_eval(data.decode("utf-8"))
+    print(ret)
+    return ret
+
+@app.route("/delete_event", methods = ["POST", "GET"])
+def delete_event():
+    res = {}
+    printit("delete_event")
+    if request.method == "POST":
+        try:
+            data = getdata(request.data)
+            id = data["ID"]
+            query = ("DELETE FROM event WHERE ID == %d"%id)
+            with sql.connect("money.db") as con:
+                cur = con.cursor()
+                cur.execute(query)
+                res = {"result : Success"}
+        except:
+            con.rollback()
+            res = {"result" : "Failure"}
+            print("Error in delete event")
+        finally:
+            con.close()
+        return json.dumps(res)
+
+
+@app.route("/changeToken", methods = ["POST","GET"])
+def change():
+    printit("changeToken")
+    res = {}
+    if request.method == "POST":
+        try:
+            data = getdata(request.data)
+            unique_id = data["unique_id"]
+            token = data["token"]
+            query = ("UPDATE user SET token = \"%s\" where unique_id == \"%s\""%(token, unique_id))
+            with sql.connect("money.db") as con:
+                cur = con.cursor()
+                cur.execute(query)
+            res = {"result" : "Success"}
+        except:
+            con.rollback()
+            res = {"result" : "Failure"}
+            print("Error in update token")
+        finally:
+            con.close()
+        return json.dumps(res)
+
+
+@app.route("/push", methods = ["POST","GET"])
+def push():
+    printit("push")
+    api =" AAAApvAniuI:APA91bFReX5VxHZdCxO6f0cAo7DkNoKnpsaEOhgi-IN-Q-qxyHqGBA2PtyLyDhs1WUQhy0eFXk_fQ7e31G-j4N9rfjNpfFlGURzLQEtFYFIUFx602FTn5sSrvt-mEivE4uXOvdB0dxIoeqCGobIX92gZm8lJ2460dQ"
+    message_title = 'Soyoung_title'
+    if request.method == 'POST':
+        try:
+            push_service = FCMNotification(api_key=api)
+            data = getdata(request.data)
+            unique_id = data["unique_id"]
+            ID = data["ID"]
+            user_query = ("SELECT * FROM user where unique_id == \"%s\""%unique_id)
+            event_query = ("SELECT * FROM event where ID == %s"%ID)
+            with sql.connect("money.db") as con:
+                cur = con.cursor()
+                for row in cur.execute(user_query):
+                    unique_id, name, nickname, account_info, token = row
+                    cur2 = con.cursor()
+                    for event in cur2.execute(event_query):
+                        ID, creditor, debtor, price, date, repayment, info = event
+                        cur3 = con.cursor()
+                        for sendtoken in cur3.execute("SELECT token from user where unique_id ==\"%s\""%debtor):
+                            real = sendtoken[0]
+                        message_body = {"name":name, "nickname":nickname, "info":info,"price":price, "date":date, "creditor_unique_id": unique_id}
+            res =push_service.notify_single_device(registration_id= real, message_title = message_title, message_body = message_body)
+            print("SENT -> %s"%res)
+        except:
+            con.rollback()
+            print("error in push. TT")
+        finally:
+            con.close()
+    return json.dumps({"result":"Success"})
 
 @app.route('/')
 def home():
-    return render_template('home.html')
-@app.route('/test', methods = ["POST","GET"])
-def test():
-    with sql.connect("money.db") as con:
-        cur = con.cursor()
-        string = "soyoung"
-        for row in cur.execute("SELECT * FROM user where name == %s"%("\""+string+"\"")):
-             unique_id, name, nickname, account_info = row
-             string =str(unique_id)+name+nickname+account_info
-             sample = [23,2]
-             return json.dumps(sample)
-    return "hello"
+    return "hi, this server is not for web browsing. please use the money app :)"
 
 @app.route('/checkuser', methods = ["POST", "GET"])
 def check_user():
+    printit("check_user")
     exists = False
     res = {}
     if request.method == 'POST':
         try:
-            raw1 = request.data
-            print(raw1)
-            raw2 = raw1.decode("utf-8")
-            print(raw2)
-            real = ast.literal_eval(raw2)
+            real = getdata(request.data)
             uid = real["unique_id"]
-            print(uid)
             with sql.connect("money.db") as con:
-                print("in checkuser")
                 cur = con.cursor()
                 query = ("SELECT * FROM user where unique_id == \"%s\""%(uid))
-                print(query)
                 for row in cur.execute(query):
                     exists = True
-                    unique_id, name, nickname, account_info = row
+                    unique_id, name, nickname, account_info, token = row
                     res = {"result" : "Success", "name" : name, "nickname": nickname, "account_info" : account_info}
-                    print("Printing res...")
-                    print (res)
-                    print("........done")
         except:
             con.rollback()
             print("Error in insert operation")
@@ -58,44 +129,25 @@ def check_user():
 
 @app.route('/make_new_user', methods = ["POST", "GET"])
 def new_user():
+    printit("new_user")
     exists = False
     if request.method == 'POST':
         try:
-            print(1)
-            raw1 = request.data
-            print(raw1)
-            print(2)
-            raw2 = raw1.decode("utf-8")
-            print(3.5)
-            real = ast.literal_eval(raw2)
-            print(real)
-            print(4)
+            real = getdata(request.data)
             uid = real["unique_id"]
-            print(uid)
-            print(5)
             name = real["name"]
-            print(name)
-            print(6)
             nickname = real["nickname"]
             account_info = real["account_info"]
-            print(7)
+            token = real["token"]
             with sql.connect("money.db") as con:
                 cur = con.cursor()
                 test = ("SELECT * FROM user where nickname == %s"%("\""+nickname+"\""))
-                print(8)
                 for row in cur.execute(test):
-                    print(9)
                     exists = True
-                print(10)
-                insert = ("INSERT INTO user (unique_id, name, nickname, account_info) VALUES (\"%s\",\"%s\",\"%s\",\"%s\")"%(uid,name,nickname,account_info))
-                print(insert)
-                print(11)
+                insert = ("INSERT INTO user (unique_id, name, nickname, account_info, token) VALUES (\"%s\",\"%s\",\"%s\",\"%s\", \"%s\")"%(uid,name,nickname,account_info, token))
                 if exists == False:
                     cur.execute(insert)
-                    print(12)
                     con.commit()
-                    print ("Record successfully added: name %s"%name)
-                print("This nickname currently already exists. please try another nickname")
         except:
             con.rollback()
             print("ERROR in insert operation")
@@ -107,16 +159,11 @@ def new_user():
 
 @app.route('/make_new_event', methods = ["POST", "GET"])
 def new_event():
+    printit("new_event")
+    res = {}
     if request.method == 'POST':
         try:
-            data = request.data
-            print(data)
-            data = data.decode("utf-8")
-            print(data)
-            print("dskksdafjlasdfk")
-            data = ast.literal_eval(data)
-            print("dasfkj;lksdajfkl;asjdkl;fasdjasdkl;fjaskld;")
-            print(data)
+            data = getdata(request.data)
             creditor = data["creditor"]
             debtor = data["debtor"]
             price = data["price"]
@@ -125,9 +172,9 @@ def new_event():
             with sql.connect("money.db") as con:
                 cur = con.cursor()
                 exe = ("INSERT INTO event (creditor, debtor, price, date, info, repayment) VALUES (\"%s\", \"%s\", %d, \"%s\", \"%s\", %d)"%(creditor, debtor, price, date, info, 0) )
-                print(exe)
                 cur.execute(exe)
                 con.commit()
+                res = {"result":"Success"}
                 print ("Record successfully added: info %s"%info)
         except:
             con.rollback()
@@ -135,32 +182,24 @@ def new_event():
             print("ERROR in insert operation")
         finally:
             con.close()
-    res = json.dumps({"result":"Success"})
-    return res
+    return (json.dumps(res))
+
 @app.route('/update_user', methods = ["POST", "GET"])
 def update_user():
-    print("in update_user")
+    printit("update_user")
     res = {}
     exists = False
     if request.method == 'POST':
         try:
-            data = request.data
-            print("DATA")
-            print(data)
-            data = data.decode("utf-8")
-            data = ast.literal_eval(data)
-            print("REALDATA")
-            print(data)
+            data = request.data(data)
             uid = data["unique_id"]
             name =  data["name"]
             nickname = data["nickname"]
             account_info = data["account_info"]
             with sql.connect("money.db") as con:
-                print('connection successful')
                 cur = con.cursor()
                 query = ("SELECT * FROM user where unique_id == \"%s\""%uid);
                 for row in cur.execute(query):
-                    print("inside select moon~~~")
                     cur.execute("UPDATE user SET name = %s,nickname = %s, account_info = %s WHERE unique_id is \"%s\""%("\""+name+"\"", "\""+nickname+"\"", "\""+account_info+"\"", uid ));
                 con.commit()
                 res = {"result": "Success"}
@@ -175,174 +214,96 @@ def update_user():
 
 @app.route('/search_by_name', methods = ["POST", "GET"])
 def search_name():
+    printit("search_name")
     if request.method == 'POST':
         try:
-            print("REQUEST start")
-            data = request.data
-            print(data)
-            print(type(data))
-            what = data.decode("utf-8")
-            print(what)
-            print(type(what))
-            yeah = ast.literal_eval(what)
-            print(yeah)
-            print(type(yeah))
-            print("did i print name?")
-            name = yeah["name"]
-            print(name)
+            data = getdata(request.data)
+            name = data["name"]
             with sql.connect("money.db") as con:
                 cur = con.cursor()
                 ret=[]
-                print("sql start:")
-
                 mystr = ("SELECT * FROM user where name LIKE \'%%%s%%\'"%(name))
-                print(mystr)
                 res = cur.execute(mystr)
-               # sql = "SELECT * FROM user where name LIKE \%%s\%"
-               # cur.execute(sql,(name))
-                print("after res")
                 for row in res:
-                    print("inside row")
-                    uid, name, nickname, account_info = row
-                    print(uid)
-                    print(name)
-                    print(nickname)
+                    uid, name, nickname, account_info, token = row
                     ret.append({"unique_id":uid, "name":name, "nickname":nickname})
-                    print(json.dumps(ret))
         except:
             con.rollback()
             print("error in search by name")
         finally:
-            print("FINAL")
             con.close()
-    print("HI")
     return json.dumps(ret)
-
-
 
 @app.route('/search_as_creditor', methods = ["POST", "GET"])
 def creditor():
+    printit("creditor")
     if request.method == 'POST':
         res = []
         try:
-            print("creditor start")
-            data = ast.literal_eval(request.data.decode("utf-8"))
-            print(data)
+            data = getdata(request.data)
             uid = data["unique_id"]
-            print("USER _ID : %s"%(uid))
             query = ("SELECT * FROM event where creditor == \"%s\""%uid)
-            print(query)
             with sql.connect("money.db") as con:
                 con = sql.connect("money.db")
                 cur = con.cursor()
-                print("IN MIDWHERE")
                 for row in cur.execute(query):
-                    print(row)
-                    print("inside cur.execute(query)")
                     ID, creditor, debtor, price, date, repayment, info = row;
                     cur2 = con.cursor()
                     helper = ("SELECT unique_id, name, nickname FROM user where unique_id == \"%s\""%debtor)
-                    print(helper)
                     for helpp in cur2.execute(helper):
                          uid, name, nickname = helpp
                          res.append({"ID":ID,"unique_id":uid,"name":name, "nickname":nickname, "price":price, "date": date, "info":info})
-                         print(res)
         except:
             con.rollback()
             print("error in search_as_creditor")
         finally:
-            print("FINAL")
             con.close()
-    print("RESPONSE : %s"%(json.dumps(res)))
     return json.dumps(res)
 
 @app.route('/find_account_info', methods = ["POST", "GET"])
 def account():
+    printit("find_account_info")
     if request.method == "POST":
-        print("REALSTART")
         try:
-            print("starting find_account_info")
-            data = ast.literal_eval(request.data.decode("utf-8"))
+            data = getdata(data)
             uid = data["unique_id"]
-            print("UID")
-            print(uid)
             query = ("SELECT account_info FROM user where unique_id == \"%s\""%(uid))
             with sql.connect("money.db")as con:
                 cur = con.cursor()
                 for row in cur.execute(query):
                     res = {"result" : "Success", "account_info":row[0]}
-                    print(res)
         except:
             res = {"result": "Failure"}
             con.rollback()
             print("error in find_account_info")
         finally:
-            print("FINAL")
             con.close()
     return json.dumps(res)
 
-
 @app.route('/search_as_debtor', methods = ["POST", "GET"])
 def debtor():
+    printit("search_as_debtor")
     if request.method == 'POST':
         res = []
         try:
-            print("debtor start")
-            data = ast.literal_eval(request.data.decode("utf-8"))
-            print(data)
+            data = getdata("request.data")
             uid = data["unique_id"]
-            #print("UID ISSSSSSSSSS")
-            #print(uid)
             query = ("SELECT * FROM event where debtor == \"%s\""%(uid))
-            #print(query)
             with sql.connect("money.db") as con:
                 cur = con.cursor()
-                #print("IN MIDWHERE")
                 for row in cur.execute(query):
-                    print("in cur.execute(query)")
                     ID, creditor, debtor, price, date, repayment, info = row;
                     cur2 = con.cursor()
-                    print("WHAT IS THE MATTER")
                     helper = ("SELECT unique_id, name, nickname FROM user where unique_id == \"%s\""%creditor)
-                    print(helper)
                     for helpp in cur2.execute(helper):
-                        print("in for loop")
                         uid, name, nickname = helpp
                         res.append({"ID":ID,"unique_id":uid, "name":name, "nickname":nickname, "price":price, "date": date, "info":info})
-                       # print(res)
         except:
             con.rollback()
             print("error in search_as_debtor")
         finally:
-            print("FINAL")
             con.close()
     return json.dumps(res)
-
-@app.route('/delete_event', methods = ["POST", "GET"])
-def delete():
-    if request.method == 'POST':
-        res = {}
-        try:
-            print("delete start.")
-            data = ast.literal_eval(request.data.decode("utf-8"))
-            print(data)
-            uid = data['ID']
-            print(uid)
-            query = ("DELETE FROM event WHERE ID == \"%s\""%(uid))
-            print(query)
-            with sql.connect("money.db") as con:
-                cur = con.cursor()
-                mid = cur.execute(query)
-                res = {"result" : "Success"}
-        except:
-            con.rollback()
-            print("error in delete")
-            res = {"result" : "Failure"}
-        finally:
-            con.close()
-    return json.dumps(res)
-
-
 
 if __name__ == '__main__':
     app.debug == True
